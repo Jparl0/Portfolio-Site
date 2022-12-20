620a9eb7cf6d821726dcd9cd5ea50a2beb1b38a1
Joseph <joeyparlamenti@yahoo.com>
2022-12-19 23:47:28 -0500

    updated github_LinkedIn link

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -86,9 +86,9 @@ function HomePage() {
               </h4>
           <div id='about-text'>
               <p id='about-para'>
-              As someone who has always had passion for games growing up, I've always been fascinated with any kind of puzzle or problem I could try to solve. 
+              As someone who has always had a passion for games growing up, I've always been fascinated with any kind of puzzle or problem I could try to solve. 
               From console games to computer games, I've always been obsessed with figuring out the most optimal ways of winning or playing. 
-              FlatIron was the perfect place to transition those passions into meaningful skills. 
+              FlatIron School's Software Engineering program was the perfect place to transition those passions into meaningful skills. 
               Leaving my previous career in Project Management to pursue Software Development has enabled me to create unique projects that revolve around my interests. 
               Data Management and optimization have always been at the forefront of how I play games and lead my professional development, so finding a position in this field is my dream.
               </p>
